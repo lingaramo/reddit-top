edbef38571bcb291af0fd91329b2ddcad2df99fd
Lucas Ingaramo <l.ingaramo@gmail.com>
2018-02-07 17:44:29 -0300

    Load redux. Create store.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger'
+import { Provider } from 'react-redux';
+
+import posts from './reducers'
+
+const loggerMiddleware = createLogger()
+
+let store = createStore(posts,
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware
+    )
+  )
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
+  document.getElementById('root'));
