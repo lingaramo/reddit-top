7136f385575d6297e78a8ada5a7bb90386af3ac2
Lucas Ingaramo <l.ingaramo@gmail.com>
2018-02-08 12:22:14 -0300

    Display selected post.

--- src/RedditPost.js
+++ src/RedditPost.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 const RedditPost = props => {
-  return(<h1>Reddit Post</h1>)
+  const { post } = props
+  if (post) {
+    return(
+      <div>
+        <p>{post.data.title}</p>
+        <h3>{post.author}</h3>
+        <img src={post.data.preview.images["0"].source.url} alt={post.title} />
+        <span>{post.num_comments} comments</span>
+      </div>
+    )
+  } else {
+    return(<h1>Reddit top post viewer</h1>)
+  }
 }
 
-export default RedditPost;
+const mapStateToProps = state => {
+  return {
+    post: state.posts.list.find(post => post.data.id === state.activePost.id)
+  }
+}
+
+export default connect(mapStateToProps)(RedditPost);
